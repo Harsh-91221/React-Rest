4f848cc28a246413b7b33b32253ecce076457028
Harsh91221 <harshch91221@gmail.com>
2024-02-25 18:50:28 +0530

    first commit

--- test.cpp
+++ test.cpp
@@ -1,37 +1,30 @@
 class Solution
 {
 public:
-    long long largestSquareArea(vector<vector<int>> &bottomLeft, vector<vector<int>> &topRight)
+    bool isPossibleDivide(vector<int> &hand, int groupSize)
     {
-        long long max_area = 0;
-        int n = bottomLeft.size();
-
-        for (int i = 0; i < n; i++)
+        sort(hand.begin(), hand.end());
+        int n = hand.size();
+        unordered_map<int, int> mp;
+        for (auto a : hand)
         {
-            for (int j = i + 1; j < n; j++)
+            mp[a]++;
+        }
+        for (auto a : hand)
+        {
+            if (mp[a] > 0)
             {
-                int minX = max(bottomLeft[i][0], bottomLeft[j][0]);
-                int minY = max(bottomLeft[i][1], bottomLeft[j][1]);
-            }
-            if (minX < maxX && minY < maxY)
-            {
-                long long part = min(maxX - minX, maxY - minY);
-                maxArea = max(maxArea, part * part);
+                for (int i = a + 1; i < a + groupSize; i++)
+                {
+                    if (mp[i] == 0)
+                    {
+                        return false;
+                    }
+                    mp[i]--;
+                }
+                mp[a]--;
             }
         }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                int maxX = min(topRight[i][0], topRight[j][0]);
-                int maxY = min(topRight[i][1], topRight[j][1]);
-            }
-            if (minX < maxX && minY < maxY)
-            {
-                long long part = min(maxX - minX, maxY - minY);
-                maxArea = max(maxArea, part * part);
-            }
-        }
-        return max_area;
+        return true;
     }
 };
