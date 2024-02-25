b9f2564d14fb7cc4962b8902887287b2362f4d30
Harsh91221 <harshch91221@gmail.com>
2024-02-25 17:43:27 +0530

    Updates

---
+++ index.1f19c6ed.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Namaste");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.1f19c6ed.js.map
