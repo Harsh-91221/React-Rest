24a0e73f696c126a7facdf8ef085b04f93dff58d
Harsh91221 <harshch91221@gmail.com>
2024-05-26 17:46:44 +0530

    CODE

--- temp.cpp
+++
@@ -1,8 +0,0 @@
-{/* <div className="flex flex-wrap">
-                {filteredRestaurant.map((restaurant) => (
-                    <Link key={restaurant.info.id} to={`/restaurants/${restaurant.info.id}`}>
-                        <RestaurantCard resData={restaurant} />
-                    </Link>
-                ))}
-            </div>
-        </div> */}
